1301e9443d1a68fb92ed4597173b8db7bcf9e76c
prajwal-26 <prajwalpatil262002@gmail.com>
2025-09-01 14:29:22 +0530

    env

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "ec2-instance" {
   ami = "ami-0cfde0ea8edd312d4"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name = "us-east-2"
 }
 output "public-ip" {
